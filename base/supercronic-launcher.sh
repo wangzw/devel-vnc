958571cd57c57eb647c8eabb5cd8e017aed376b0
Zhanwei Wang <wang@hashdata.cn>
2026-03-29 19:50:06 +0800

    Base: fix supercronic-launcher using absolute path to avoid PATH lookup failure

--- base/supercronic-launcher.sh
+++ base/supercronic-launcher.sh
@@ -7,4 +7,4 @@ CRONTAB_DIR=/etc/supercronic.d
 # Assemble crontab from fragments
 cat "$CRONTAB_DIR"/*.crontab > "$CRONTAB" 2>/dev/null || truncate -s 0 "$CRONTAB"
 
-exec supercronic "$CRONTAB"
+exec /usr/local/bin/supercronic "$CRONTAB"
